f9b958f4e39374721aa85d935b294e3682ec223e
Jose <gccinane@gmail.com>
2020-11-25 10:56:27 -0300

    changes to baseurl

--- mobile/src/components/ReflectionCard/index.tsx
+++ mobile/src/components/ReflectionCard/index.tsx
@@ -23,39 +23,10 @@ const ReflectionCard: React.FC<ReflectionCardParams> = ({ text, iconName, isDate
   const [visibleActions, setVisibleActions] = useState(false)
   return (
     <CardContainer>
-      <CardItem isDateCard={isDateCard} cardColor={cardColor} >
+      <CardItem isDateCard cardColor={cardColor} >
         <CardText>
           {text}
         </CardText>
-        {iconName &&
-        <>
-          <CardIcon name={iconName} size={30} style={{ position: 'absolute', right: 2 }} onPress={() => setVisibleActions(!visibleActions)}/>
-          {visibleActions
-            ? <ActionContainer style={{ position: 'absolute' }}>
-              <TouchableOpacity onPress={() => executeDelete()}>
-                <ActionItem>
-                  <CardIcon name="trash-can-outline" size={30} style={{ color: 'red' }}/>
-                  <ActionText>
-                    Excluir
-                  </ActionText>
-                </ActionItem>
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => executeUpdate({ content: item.content, item: item.id, created_at: item.created_at })}>
-                <ActionItem>
-                  <CardIcon name="comment-edit" size={30} style={{ color: '#7159c1', right: 3 }}/>
-                  <ActionText>
-                    Editar
-                  </ActionText>
-                </ActionItem>
-              </TouchableOpacity>
-
-            </ActionContainer> : null
-          }
-
-        </>
-        }
-
       </CardItem>
     </CardContainer>
   )
